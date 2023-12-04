ecdc5fe9e3a8130cc4fc1df40317d7a42cf53abe
zapletnyuky <uliazapletnuk@MacBook-Air-Ulia.local>
2023-12-04 19:52:45 +0200

    initializing project

---
+++ src/test/java/com/example/pizza_service/PizzaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizza_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
