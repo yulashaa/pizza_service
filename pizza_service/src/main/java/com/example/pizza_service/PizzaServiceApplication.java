c7bcbb654077afe9ac8cf1a315225cf2cf4760d8
yulashaa <115156342+yulashaa@users.noreply.github.com>
2023-12-14 19:55:14 +0200

    initializing project

---
+++ pizza_service/src/main/java/com/example/pizza_service/PizzaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizza_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaServiceApplication.class, args);
+	}
+
+}
