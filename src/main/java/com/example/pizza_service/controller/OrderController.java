6c238f394ef2741fdd9e01378c399eaac01ca3c8
yulashaa <115156342+yulashaa@users.noreply.github.com>
2023-12-14 22:05:56 +0200

    Update OrderController.java

--- src/main/java/com/example/pizza_service/controller/OrderController.java
+++ src/main/java/com/example/pizza_service/controller/OrderController.java
@@ -17,19 +17,16 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    //confirm orderx
     @PostMapping("/confirm") //body {"id": 1}
     public ResponseEntity confirmOrder(@Valid @RequestBody IdDTO idDTO) {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.confirmOrder(idDTO.getId()));
     }
 
-    //get order
     @GetMapping("/{id}")
     public ResponseEntity getOrder(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getOrder(id));
     }
 
-    //get user orders
     @GetMapping("/user/{id}")
     public ResponseEntity getUserOrders(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(orderService.getUserOrders(id));
